8e4c74b3e2e492257c9df091bb98225141cc1fdb
taylorjbrown <taylorjbrown1234@gmail.com>
2018-04-27 17:45:07 -0400

    starting to get somewhere

---
+++ DeveloperEvaluation/zApp/StatsCalc.js
@@ -0,0 +1,25 @@
+﻿var App = angular.module("StatsCalc",["ui.router"]);
+
+//App.service("stackTraceService", stackTraceService);
+
+App.controller("statsCalcController", statsCalcController);
+
+var configFun = function ($stateProvider, $urlProvider, $locationProvider) {
+    $locationProvider.html5mode(true);
+    $stateProvider
+        .state("StatCalcView", {
+            url: "/Stats",
+            templateUrl: "zApp/components/Stats/StatsCalcView.html",
+            controller: statsCalcController
+        }
+    )
+}
+
+configFun.$inject = ['$stateProvider', '$urlProvider', '$locationProvider'];
+
+App.run(["$rootScope", "$state", function ($rootScope, $state) {
+    $rootScope.$state = $state;
+    if ($rootScope.$state !== "StatCalcView") {
+        $state.go("StatCalcView");
+    }
+}]);
---
+++ DeveloperEvaluation/zApp/components/Stats/StatsCalcController.js
@@ -0,0 +1,7 @@
+﻿
+var statsCalcController = function ($scope, $http) {
+    $scope.test = 'here';
+    console.log('in controller');
+}
+
+statsCalcController.$inject = ['$scope', '$http'];

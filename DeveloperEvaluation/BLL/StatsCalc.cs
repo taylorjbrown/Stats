b15a27fab0daf3a208ed794fa72b58fe6d82a816
taylorjbrown <taylorjbrown1234@gmail.com>
2018-04-29 21:48:32 -0400

    no errors on the frontend, backend looks clean, test work

--- DeveloperEvaluation/BLL/StatsCalc.cs
+++ DeveloperEvaluation/BLL/StatsCalc.cs
@@ -21,47 +21,30 @@ namespace DeveloperEvaluation.BLL
             }
             else
             {
-                List<decimal> two = nums.OrderBy(n => n).Skip( (nums.Count / 2)-1).Take(2).ToList<decimal>();
+                List<decimal> two = nums.OrderBy(n => n).Skip( (nums.Count / 2)-1).Take(2).ToList();
                 return ((two[0] + two[1]) / 2M);
             }
         }
 
         public async Task<List<decimal>> mode(List<decimal> nums)
         {
+
             var keycounts = nums.GroupBy(n => n)
                .Select(g => new { val = g.Key, count = g.Count() })
                .OrderByDescending(n => n.count)
-               .Where(n=> n.count > 1)
                .ToList();
 
-            List<decimal> result = new List<decimal>();
+            int maxCount = keycounts[0].count;
 
-            if (keycounts.Count == 0)
-            {
-                return result;
-            }
+            List<decimal> result = keycounts
+                .Where(x => x.count == maxCount)
+                .Select(x=>x.val)
+                .ToList();
 
-            var prev = keycounts[0].count;
+            int numsCount = nums.Count;
+            nums.Clear();
 
-          
-            result.Add(keycounts[0].val);
-
-            foreach (var item in keycounts.Skip(1))
-            {
-                if(prev == item.count)
-                {
-                    result.Add(item.val);
-                }
-            }
-
-            if (result.Count != nums.Count)
-            {
-                return result;
-            }
-            else
-            {
-                return new List<decimal>();
-            }
+            return (result.Count != numsCount) ? result : nums;
         }
 
         public async Task<Stats> CalcAsync(List<decimal> nums)
